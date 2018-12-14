58bf7da1cc47c8f303b3d8c901dbbc8069b558bd
Dan Cartier <nospamdan1@gmail.com>
2018-12-14 15:02:10 -0500

    Revert "msm8998: pocketmode: use org.lineageos.platform"
    This reverts commit 3785f8735301d401aa16fd44a49884b21b9176cd.

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -11,7 +11,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform
+    org.cyanogenmod.platform
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
