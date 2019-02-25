cea2c63ee13ef27be185e6f587d61059731d3024
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-02-25 17:58:19 +0100

    msm8998-common: build versioned vndk
    Change-Id: I0a3d40619819e45f8193d20a632fe36714f3d057

--- common.mk
+++ common.mk
@@ -458,13 +458,8 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     vndk_package
 
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so
-
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk-sp
+#PRODUCT_COPY_FILES += \
+#    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so
 
 # VR
 PRODUCT_PACKAGES += \
