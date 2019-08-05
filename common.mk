LineageOS/android_device_oneplus_msm8998-common

msm8998-common: fix oneplus typo on security patch date
Change-Id: If1d1837a6c25935680ab1faea58d8aa4449aa5f0

common.mk
@@ -439,7 +439,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2018-06-05
+    ro.lineage.build.vendor_security_patch=2019-06-05
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system

NoSpamDan/device_oneplus_msm8998-common

common.mk
@@ -435,7 +435,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.candy.build.vendor_security_patch=2018-06-05
+    ro.candy.build.vendor_security_patch=2019-06-05
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system