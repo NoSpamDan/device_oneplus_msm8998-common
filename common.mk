LineageOS/android_device_oneplus_msm8998-common

msm8998-common: update blobs from 9.0.7
Change-Id: Id161a1797d08142b32fb4d02ca483275d829dfd4

common.mk
@@ -439,7 +439,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2019-06-05
+    ro.lineage.build.vendor_security_patch=2019-06-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system

NoSpamDan/device_oneplus_msm8998-common

common.mk
@@ -435,7 +435,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.candy.build.vendor_security_patch=2019-06-05
+    ro.candy.build.vendor_security_patch=2019-06-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system