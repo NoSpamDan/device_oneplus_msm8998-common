crdroidandroid/android_device_oneplus_msm8998-common

msm8998-common: Update lineage vendor security prop
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

common.mk
@@ -460,7 +460,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2019-08-01
+    ro.lineage.build.vendor_security_patch=2019-10-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system

NoSpamDan/device_oneplus_msm8998-common

Change-Id: I87ce578aa5a793cdad7aeaa4f11f2c9551fd6042

common.mk
@@ -455,7 +455,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.candy.build.vendor_security_patch=2019-08-01
+    ro.candy.build.vendor_security_patch=2019-10-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system