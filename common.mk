14aaf0e79916a410224cb75b60b887f479ab7ccf
NoSpamDan <nospamdan1@gmail.com>
2019-12-07 20:41:52 -0500

    msm8998-common: Drop '-qti' power HAL suffix

--- common.mk
+++ common.mk
@@ -356,7 +356,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
